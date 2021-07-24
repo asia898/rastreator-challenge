e175ce0fb166448626ed95d884abda7f40e279e1
asia898 <joanna.kwiatkowska989@gmail.com>
2021-07-24 12:07:21 +0200

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import bar from './card.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style.css';
+bar();
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  watch: true,
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
